b56e22eb83eca3037148393fa67a978dd1610d39
Csaba Moldovan <moldovan@Csabas-MacBook-Air.local>
2025-02-08 21:10:52 +0100

    Update backend index.js

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-mongoose.connect('mongodb+srv://moldovancsaba:gF0LnQqmK89pYW79@customato-prototype-clu.rwf9g.mongodb.net/?retryWrites=true&w=majority&appName=customato-prototype-cluster')
-  .then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
